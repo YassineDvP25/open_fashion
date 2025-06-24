23cec2c55983bf22859a3c0e7ce30a59d2c0b8e5
Yassine El Anssari <yassinedvp25@gmail.com>
2025-06-24 10:05:27 +0100

    feat(app): add initial theming, assets, and UI setup

---
+++ lib/core/helpers/spacing.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+SizedBox  verticalSpace(double height) {
+  return SizedBox(height: height.h);
+}
+SizedBox horizantalSpace(double width) {
+  return SizedBox(width: width.w);
+}

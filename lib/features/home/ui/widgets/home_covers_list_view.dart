108c8b7221912924969f619a26246fe5298515da
Yassine El Anssari <yassinedvp25@gmail.com>
2025-06-26 11:04:11 +0100

    refactor(home): update product visuals and improve UI components

--- lib/features/home/ui/widgets/home_covers_list_view.dart
+++ lib/features/home/ui/widgets/home_covers_list_view.dart
@@ -1,14 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:open__fashion__app/core/helpers/spacing.dart';
+import 'package:open__fashion__app/features/home/data/covers_model.dart';
 
 class HomeCoversListView extends StatelessWidget {
   const HomeCoversListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return Container();
-      },
+    return SizedBox(
+      height: 460.h,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: CoverModel.covers.length,
+        itemBuilder: (context, index) {
+          var cover = CoverModel.covers[index];
+          return Column(
+            children: [
+              Expanded(
+                flex: 10,
+                child: Container(
+                  height: 200.h,
+                  width: 310.w,
+                  padding: EdgeInsets.symmetric(horizontal: 10).w,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30)
+                  ),
+                  
+                  child: Container(
+                    width: 270, // نفس العرض من Figma
+                    height: 300, // نفس الارتفاع من Figma
+                    decoration: BoxDecoration(
+                      color: Colors.black, // خلفية سوداء مثل التصميم
+                      image: DecorationImage(
+                        image: AssetImage(
+                          cover.image,
+                        ), // ضع مسار الصورة الصحيح هنا
+                        fit:
+                            BoxFit
+                                .fill, // يضمن تغطية الحاوية بالكامل مثل التصميم
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              verticalSpace(10),
+
+              Expanded(
+                flex: 1,
+                child: Text(
+                  cover.name.toUpperCase(),
+                  style: TextStyle(
+                    fontFamily: 'TenorSans',
+                    color: Colors.white,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 1.5,
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
--- lib/features/home/ui/widgets/home_product.dart
+++ lib/features/home/ui/widgets/home_product.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:open__fashion__app/core/components/text.dart';
 import 'package:open__fashion__app/core/helpers/spacing.dart';
 import 'package:open__fashion__app/features/home/data/product_model.dart';
+import 'package:open__fashion__app/features/home/ui/widgets/home_product_price_and_rating.dart';
 
 class HomeProduct extends StatelessWidget {
   final int index;
@@ -15,27 +16,34 @@ class HomeProduct extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        
         Container(
-          clipBehavior: Clip.antiAlias,
+          width: 200.w,
+          height: 210.h,
           decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(5.r ),
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              image: AssetImage(product.image),
+              fit: BoxFit.fill,
+            ),
           ),
-    
-          child: Image.asset(product.image),
         ),
+
         verticalSpace(10),
-        CustomText(text: product.name, fontWeight: FontWeight.bold),
-        verticalSpace(5),
         CustomText(
           text: product.description,
           color: Color(0xffF9F9F9),
           fontSize: 10.sp,
         ),
-    
         verticalSpace(10),
-        CustomText(text: '\$${product.price}', fontSize: 15.sp),
+        CustomText(text: product.name, fontWeight: FontWeight.bold),
+        verticalSpace(5),
+
+        HomeProductPriceAndRating(product: product),
+        Divider(
+          color: Colors.white.withValues(alpha: 0.3),
+          thickness: 0.5,
+          height: 20.h,
+        ),
       ],
     );
   }
